0f079de6a63859db3f9f7cb1ffcdcaeed25cc3e8
Sean McKibben <graphex@graphex.com>
2021-05-16 23:14:15 -0600

    All anodes settable
    next step is simple continuous time

--- src/clock_objects.rs
+++ src/clock_objects.rs
@@ -6,15 +6,80 @@ use std::time::Duration;
 extern crate typenum;
 
 use bit_array::{BitArray, BitsIn};
-use typenum::{Unsigned, U96};
+use bit_vec::BitVec;
+use typenum::{Unsigned, U96, U10, U2};
 use rppal::spi::{Bus, Mode, Segment, SlaveSelect, Spi};
 use rppal::gpio::{Gpio, OutputPin};
 use rppal::system::DeviceInfo;
 use std::mem::replace;
 use self::typenum::NonZero;
 use std::ops::{Add, Sub, Div};
+use std::iter::FromIterator;
+use std::borrow::BorrowMut;
 
+//The latch enable pin GPIO number. Should be low during writes.
 const LE_PIN: u8 = 22;
+
+//TODO: use as generic type parameter for DisplayMessages and ClockDisplay
+pub enum ClockType {
+    _9Tubes,
+    _6Tubes,
+}
+
+pub enum DisplayElement {
+    NumericTube(NumericTube),
+    Separator(Separator),
+    IN19ATube(IN19ATube),
+}
+
+pub struct DisplayMessage {
+    pub t0:NumericTube,
+    pub t1:NumericTube,
+    pub s0:Separator,
+    pub t2:NumericTube,
+    pub t3:NumericTube,
+    pub s1:Separator,
+    pub t4:NumericTube,
+    pub t5:NumericTube,
+    pub s2:Separator,
+    pub t6:NumericTube,
+    pub t7:NumericTube,
+    pub t8:IN19ATube,
+}
+
+impl DisplayMessage {
+    // 0-1 s2
+    // 2-11 t8
+    // 12-21 t7
+    // 22-31 t6
+    // 32-33 s1
+    // 34-43 t5
+    // 44-53 t4
+    // 54-63 t3
+    // 64-65 s0
+    // 66-75 t2
+    // 76-85 t1
+    // 86-95 t0
+    fn to_raw(&self) -> BitArray::<u8, U96> {
+        // let mut combined_vec:BitVec<u8> = BitVec::<u8>::new();
+        // combined_vec.reserve(96);
+        // combined_vec.append(&mut self.s2.get_bits().iter().copied().collect());
+        vec![
+            self.s2.get_bits().iter(),
+            self.t8.get_bits().iter(),
+            self.t7.get_bits().iter(),
+            self.t6.get_bits().iter(),
+            self.s1.get_bits().iter(),
+            self.t5.get_bits().iter(),
+            self.t4.get_bits().iter(),
+            self.t3.get_bits().iter(),
+            self.s0.get_bits().iter(),
+            self.t2.get_bits().iter(),
+            self.t1.get_bits().iter(),
+            self.t0.get_bits().iter(),
+        ].into_iter().flatten().collect()
+    }
+}
 
 #[derive(Debug)]
 pub struct ClockDisplay {
@@ -35,9 +100,13 @@ impl ClockDisplay {
         Ok(cd)
     }
     fn write_frame(&mut self) {
-        self.le_pin.set_low();
-        self.spi.write(&*self.raw_message.to_bytes());
-        self.le_pin.set_high();
+        if self.le_pin.is_set_low() {
+            println!("Latch already set low by another process, aborting write!")
+        } else {
+            self.le_pin.set_low();
+            self.spi.write(&*self.raw_message.to_bytes());
+            self.le_pin.set_high();
+        }
     }
     pub fn sweep(&mut self) {
         for i in 0..96 {
@@ -47,6 +116,10 @@ impl ClockDisplay {
             self.write_frame();
             thread::sleep(Duration::from_millis(50));
         }
+    }
+    pub fn show(&mut self, dm:DisplayMessage) {
+        self.raw_message = dm.to_raw();
+        self.write_frame();
     }
 }
 
@@ -67,43 +140,126 @@ impl ClockDisplay {
 // };
 
 
-// 0-1 s2
-// 2-11 t8 _ _ ℃ μ η κ ₘ Ρ Μ % (IN-19A)
-// 12-21 t7
-// 22-31 t6
-// 32-33 s1
-// 34-43 t5
-// 44-53 t4
-// 54-63 t3
-// 64-65 s0
-// 66-75 t2
-// 76-85 t1
-// 86-95 t0
 
-// pub trait Tube {
-//     fn get_bits(&self) -> impl BitArray;
-//     // fn set_bits(&mut self, BitArray<u8, U10>);
-// }
-//
-// pub struct NumericTube {
-//     bits: BitArray<u8, U10>,
-// }
-//
-// impl Tube for NumericTube {
-//     fn get_bits(&self) -> impl BitArray {
-//         self.bits.clone()
-//     }
-// }
-//
-// pub enum NumericBitsIndex {
-//     C0 = 9,
-//     C1 = 8,
-//     C2 = 7,
-//     C3 = 6,
-//     C4 = 5,
-//     C5 = 4,
-//     C6 = 3,
-//     C7 = 2,
-//     C8 = 1,
-//     C9 = 0,
-// }
+
+pub trait Tube {
+    fn get_bits(&self) -> BitVec<u8>;
+}
+
+pub struct NumericTube {
+    bits: BitArray::<u8, U10>,
+}
+
+impl Tube for NumericTube {
+    fn get_bits(&self) -> BitVec<u8> {
+        BitVec::<u8>::from_iter(self.bits.iter())
+    }
+}
+
+impl NumericTube {
+    pub fn new(bit_index: NumericBitsIndex) -> NumericTube {
+        let mut tube:NumericTube = NumericTube {
+            bits: BitArray::<u8, U10>::from_elem(false)
+        };
+        tube.set_cathode(bit_index);
+        tube
+    }
+    pub fn set_cathode(&mut self, bit_index: NumericBitsIndex) {
+        self.bits.clear();
+        match bit_index{
+            NumericBitsIndex::BLANK => (),
+            _ => self.bits.set(bit_index as usize, true),
+        }
+    }
+}
+
+pub enum NumericBitsIndex {
+    _0 = 9,
+    _1 = 8,
+    _2 = 7,
+    _3 = 6,
+    _4 = 5,
+    _5 = 4,
+    _6 = 3,
+    _7 = 2,
+    _8 = 1,
+    _9 = 0,
+    BLANK = 10,
+}
+
+pub struct IN19ATube {
+    bits: BitArray::<u8, U10>,
+}
+
+impl Tube for IN19ATube {
+    fn get_bits(&self) -> BitVec<u8> {
+        BitVec::<u8>::from_iter(self.bits.iter())
+    }
+}
+
+impl IN19ATube {
+    pub fn new(bit_index: IN19ABitsIndex) -> IN19ATube {
+        let mut tube:IN19ATube = IN19ATube {
+            bits: BitArray::<u8, U10>::from_elem(false)
+        };
+        tube.set_cathode(bit_index);
+        tube
+    }
+
+    fn set_cathode(&mut self, bit_index: IN19ABitsIndex) {
+        self.bits.clear();
+        match bit_index{
+            IN19ABitsIndex::BLANK => (),
+            _ => self.bits.set(bit_index as usize, true),
+        }
+    }
+}
+
+pub enum IN19ABitsIndex {
+    //_ _ ℃ μ η κ ₘ Ρ Μ % (IN-19A)
+    CELSIUS = 2,
+    MICRO = 3,
+    NANO = 4,
+    KELVIN = 5,
+    M_SMALL = 6,
+    P = 7,
+    M = 8,
+    PERCENT = 9,
+    BLANK = 10,
+}
+
+pub struct Separator {
+    bits: BitArray::<u8, U2>,
+}
+
+impl Tube for Separator {
+    fn get_bits(&self) -> BitVec<u8> {
+        BitVec::<u8>::from_iter(self.bits.iter())
+    }
+}
+
+impl Separator {
+    pub fn new(bit_index: SeparatorBitsIndex) -> Separator {
+        let mut tube:Separator = Separator {
+            bits: BitArray::<u8, U2>::from_elem(false)
+        };
+        tube.set_indicators(bit_index);
+        tube
+    }
+
+    fn set_indicators(&mut self, bit_index: SeparatorBitsIndex) {
+        self.bits.clear();
+        match bit_index{
+            SeparatorBitsIndex::BLANK => (),
+            SeparatorBitsIndex::BOTH => self.bits.negate(),
+            _ => self.bits.set(bit_index as usize, true),
+        }
+    }
+}
+
+pub enum SeparatorBitsIndex {
+    TOP = 0,
+    BOTTOM = 1,
+    BOTH,
+    BLANK,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
 use std::error::Error;
 
-
 use crate::clock_objects::*;
-use bit_array::BitArray;
-
 mod clock_objects;
 
 use rppal::spi::{Bus, Mode, Segment, SlaveSelect, Spi};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut clock = ClockDisplay::new()?;
-    loop {
-        clock.sweep();
-    }
+    // loop {
+    //     clock.sweep();
+    // }
+    clock.show(DisplayMessage{
+        t0: NumericTube::new(NumericBitsIndex::_0),
+        t1: NumericTube::new(NumericBitsIndex::_1),
+        s0: Separator::new(SeparatorBitsIndex::BOTH),
+        t2: NumericTube::new(NumericBitsIndex::_2),
+        t3: NumericTube::new(NumericBitsIndex::_3),
+        s1: Separator::new(SeparatorBitsIndex::BOTH),
+        t4: NumericTube::new(NumericBitsIndex::_4),
+        t5: NumericTube::new(NumericBitsIndex::_5),
+        s2: Separator::new(SeparatorBitsIndex::BOTH),
+        t6: NumericTube::new(NumericBitsIndex::_6),
+        t7: NumericTube::new(NumericBitsIndex::_7),
+        t8: IN19ATube::new(IN19ABitsIndex::CELSIUS),
+    });
     Ok(())
 }
 
