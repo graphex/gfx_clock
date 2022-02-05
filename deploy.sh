408981d744b4a76a0c9399f2c058cdcc8189c871
Sean McKibben <graphex@graphex.com>
2022-02-05 14:04:25 -0700

    deploying as a systemd service

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,21 @@ set -o pipefail
 set -o xtrace
 
 readonly PROJECT_NAME=gfx_clock
-readonly TARGET_HOST=pi@clockpi
+readonly TARGET_HOST=pi@192.168.168.202
 readonly TARGET_PATH=/home/pi/gfx_clock
 readonly ARGS=NCS3148C
 readonly TARGET_ARCH=armv7-unknown-linux-gnueabihf
 #readonly TARGET_ARCH=arm-unknown-linux-musleabi
 readonly SOURCE_PATH=./target/${TARGET_ARCH}/release/gfx_clock
+readonly SOURCE_SERVICE=./service/gfx_clock.service
+readonly TARGET_SERVICE=/lib/systemd/system/gfx_clock.service
 
 docker run -v "$(pwd)":"/root/workspace" -a stdout -a stderr rustpi
 rsync ${SOURCE_PATH} ${TARGET_HOST}:${TARGET_PATH}
-ssh -t ${TARGET_HOST} ${TARGET_PATH} ${ARGS}
+
+# put the /service/gfx_clock.service in the pi's /lib/systemd/system/ dir
+rsync --rsync-path="sudo rsync" ${SOURCE_SERVICE} ${TARGET_HOST}:${TARGET_SERVICE}
+ssh -t ${TARGET_HOST} "sudo chmod 644 ${TARGET_SERVICE} && sudo systemctl daemon-reload && sudo systemctl enable gfx_clock.service && sudo systemctl restart gfx_clock.service"
+ssh -t ${TARGET_HOST} journalctl -f -u gfx_clock.service --no-pager
+
+# ssh -t ${TARGET_HOST} ${TARGET_PATH} ${ARGS}
